075247ce604693d1e531fe51ae81382e752b88cb
Nehliin <onehlin@kth.se>
2017-08-01 21:45:28 +0200

    New version of freeweights

--- code/freeweights.cpp
+++ code/freeweights.cpp
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <iostream>
 #include <cmath>
 
 inline void fastRead_int(int &x) {
-    register int c = getchar();
+    register int c = getchar_unlocked();
     x = 0;
-    for(; ((c<48 || c>57)); c = getchar());
-    for(; c>47 && c<58 ; c = getchar()) {
+    for(; ((c<48 || c>57)); c = getchar_unlocked());
+    for(; c>47 && c<58 ; c = getchar_unlocked()) {
     	x = (x<<1) + (x<<3) + c - 48;
     }
 }
@@ -15,77 +14,75 @@ int main(){
     int n;
     int *row1;
     int *row2;
-    int *middle;
     fastRead_int(n);
     row1 = new int[n];
     row2 = new int[n];
-    int heaviest = 0;
-    int start = 0;
-    int end = -1;
-    bool separated = false;
-    bool first = true;
+    int firstIndex, secondIndex = 0;
+    int firstMove, secondMove = 0;
+    int neut = 1;
+    bool foundPair = false;
     fastRead_int(row1[0]);
+    int firstHeavy = row1[0];
     for(int i = 1; i < n; ++i){
         fastRead_int(row1[i]);
         if(row1[i] == row1[i-1]){
+            if(firstHeavy == row1[i]){
+                firstHeavy = 0;
+            }
             row1[i] = -row1[i];
             row1[i-1] = -row1[i];
             continue;
         }
-        if(row1[i-1] > heaviest){
-            heaviest = row1[i-1];
-            start = i-1;
+        if(row1[i] > firstHeavy){
+            firstHeavy = row1[i];
+            firstIndex = i;
         }else{
-            if(row1[i-1] == heaviest){
-                end = i-1;
+            if(row1[i] == firstHeavy){
+                foundPair = true;
+            }else{
+                if(row1[i] > firstMove && !foundPair){
+                    firstMove = row1[i];
+                }
             }
         }
-
     }
+    foundPair = false;
     fastRead_int(row2[0]);
-    //std::cout << row2[0] << std::endl;
+    int secondHeavy = row2[0];
     for(int i = 1; i < n; ++i){
         fastRead_int(row2[i]);
-        //std::cout << row2[i] << std::endl;
         if(row2[i] == row2[i-1]){
+            if(secondHeavy == row2[i]){
+                secondHeavy = 0;
+            }
             row2[i] = -row2[i];
-            row2[i-1] = -row2[i-1];
+            row2[i-1] = -row2[i];
+
             continue;
         }
-        if(row2[i-1] > heaviest){
-            if(end == -1){
-                separated = true;
-            }
-            heaviest = row2[i-1];
-            start = i-1;
-            first = false;
+        if(row2[i] > secondHeavy){
+            secondHeavy = row2[i];
+            secondIndex = i;
         }else{
-            if(row2[i-1] == heaviest && !separated){
-                end = i-1;
+            if(row2[i] == secondHeavy){
+                foundPair = true;
+            }else{
+                if(row2[i] > secondMove && !foundPair){
+                    secondMove = row2[i];
+                }
             }
         }
     }
-    int max = 0;
-    if(separated){
-        printf("%d",heaviest);
+    if(secondHeavy == firstHeavy){
+        printf("%d",firstHeavy);
         return 0;
     }
-    std::cout << first << std::endl;
-    std::cout << start << std::endl;
-    std::cout << end << std::endl;
-    if(first){
-        for(int i = start; i < end; ++i){
-            if(std::abs(row1[i]) > max){
-                max = std::abs(row1[i]);
-            }
-        }
+    if(firstHeavy > secondHeavy){
+        printf("%d",firstMove);
+        return 0;
     }else{
-        for(int i = start; i < end; ++i){
-            if(std::abs(row2[i]) > max){
-                max = std::abs(row2[i]);
-            }
-        }
+        printf("%d",secondMove);
+        return 0;
     }
-    printf("%d",max);
-    return 0;
+
 }
